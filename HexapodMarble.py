121861eca8a6b40bba427fbe5ae699f6c6e4161e
Jacob Mader <91611605+MAJMAD@users.noreply.github.com>
2021-12-16 18:59:07 +0000

    Update HexapodMarble.py
    cleaned

--- HexapodMarble.py
+++ HexapodMarble.py
@@ -1,6 +1,5 @@
 from signal import signal, SIGTERM, SIGHUP, pause
 from time import sleep
-#import RPi.GPIO as GPIO
 from gpiozero import DistanceSensor, LED
 from threading import Thread
 from pipython.pidevice.interfaces.piserial import PISerial
@@ -10,8 +9,6 @@ from pipython.pidevice.gcscommands import GCSCommands
 reading = True
 Usensor = DistanceSensor(echo=15, trigger=14)
 Vsensor = DistanceSensor(echo=5, trigger=0)
-CONTROLLERNAME = 'C-887'
-STAGES = None  # this controller does not need a 'stages' setting
 REFMODES = 'FRF'
 AXES = ["X", "Y", "Z", "U", "V", "W"]
 HOME = [0,0,0,0,0,0]
@@ -22,17 +19,6 @@ bluePin1 = LED(17)
 redPin2 = LED(20)
 greenPin2 = LED(16) 
 bluePin2 = LED(21)
-
-# def blink(pin):
-#     GPIO.setmode(GPIO.BOARD)
-#     
-#     GPIO.setup(pin, GPIO.OUT)
-#     GPIO.output(pin, GPIO.HIGH)
-#     
-# def turnOff(pin):
-#     GPIO.setmode(GPIO.BOARD)
-#     GPIO.setup(pin, GPIO.OUT)
-#     GPIO.output(pin, GPIO.LOW)
     
 def redOn():
     redPin1.on()
@@ -86,13 +72,11 @@ def read_distance(pidevice, Umaxdist, Vmaxdist):
     UArray = [0, 0, 0, 0, 0]
     VArray = [0, 0, 0, 0, 0]
     while reading:
-        #print(truncate(pidevice.qPOS('X')['X'], 4), truncate(pidevice.qPOS('Y')['Y'], 4), truncate(pidevice.qPOS('Z')['Z'], 4), truncate(pidevice.qPOS('U')['U'], 4), truncate(pidevice.qPOS('V')['V'], 4), truncate(pidevice.qPOS('W')['W'], 4))
         sleep(0.2)
         lock = 0
         for value in range(0,4):
             UArray[value] = UArray[value+1]
             VArray[value] = VArray[value+1]
-            #print(value)
         UArray[4] = Usensor.value
         VArray[4] = Vsensor.value
         Uval = sum(UArray)/5
@@ -109,105 +93,58 @@ def read_distance(pidevice, Umaxdist, Vmaxdist):
             greenPin2.off()
             redPin2.on()
             lock += 10
-#         print(pidevice.qVMO(('U', 'V'), (Umaxdist*(Uval-0.5),Vmaxdist*(Vval-0.5))))
         if pidevice.qVMO(('U', 'V'), (Umaxdist*(Uval-0.5),Vmaxdist*(Vval-0.5))) == False:
-#             tar = pidevice.qTRA(('U', 'V'), (Umaxdist*(Uval-0.5),Vmaxdist*(Vval-0.5)))
-#             tar['U'], tar['V'] = truncate(tar['U'], 2), truncate(tar['V'], 2)
             badmoves = 1
-            
         if lock == 0 and badmoves == 0:
             redOff()
             blueOff()
             greenOn()
-#             print("lock = 0 badmoves = 0")
-#             print("U Distance: " + "{:1.2f}".format(Uval) + " m")
-#             print("Moving U to: " + "{:1.2f}".format(Umaxdist*(Uval-0.5)))
-#             print("V Distance: " + "{:1.2f}".format(Vval) + " m")
-#             print("Moving V to: " + "{:1.2f}".format(Vmaxdist*(Vval-0.5)))
-#             print(Umaxdist*(Uval-0.5),Vmaxdist*(Vval-0.5))
             pidevice.MOV(('U','V'), (Umaxdist*(Uval-0.5), Vmaxdist*(Vval-0.5)))#
             WaitForMotionDone(pidevice, 'U')
             WaitForMotionDone(pidevice, 'V')
-#             print("Finished Good Moves")
-            #sleep(0.2)
         if lock == 0 and badmoves == 1:
             redOff()
             blueOff()
             greenOn()
             tar = pidevice.qTRA(('U', 'V'), (Umaxdist*(Uval-0.5),Vmaxdist*(Vval-0.5)))
-           # print(tar)
             tar['U'], tar['V'] = truncate(tar['U'], 1), truncate(tar['V'], 1)
-           # print(tar)
-           # print("lock = 0 badmoves = 1")
-           # print("U Distance: " + "{:1.2f}".format(Uval) + " m")
-           # print("Moving U to: " + "{:1.2f}".format(tar['U']))
-           # print("V Distance: " + "{:1.2f}".format(Vval) + " m")
-            #print("Moving V to: " + "{:1.2f}".format(tar['V']))
-           # print(Umaxdist*(Uval-0.5),Vmaxdist*(Vval-0.5))
             pidevice.MOV(('U','V'), (tar['U'], tar['V']))
             WaitForMotionDone(pidevice, 'U')
             WaitForMotionDone(pidevice, 'V')
-#             print("Finished Good Moves")
-            #sleep(0.2)
         elif lock == 1:
             bluePin2.off()
             redPin2.off()
             greenPin2.on()
-#             print("lock = 1 Deadzoning U")
             goodmove = pidevice.qVMO(('V',), (Vmaxdist*(Vval-0.5),))
             if goodmove == False:
-                #print(pidevice.qPOS())
                 tar = pidevice.qTRA(('V',), (Vmaxdist*(Vval-0.5),))
-                #print(tar)
                 tar['V'] = truncate(tar['V'], 1)
-                #print(tar)
-                #print("V Distance: " + "{:1.2f}".format(Vval) + " m")
-                #print("Moving V to: " + "{:1.2f}".format(tar['V']))
                 pidevice.MOV(('V'),(tar['V'])) ###############errored out
                 WaitForMotionDone(pidevice, 'V')
             else:
-#                 print("V Distance: " + "{:1.2f}".format(Vval) + " m")
-                #print("Moving V to: " + "{:1.2f}".format(tar['V']))
                 pidevice.MOV(('V',),(Vmaxdist*(Vval-0.5),))
                 WaitForMotionDone(pidevice, 'V')
-                
-#             print("Finished Deadzone U Moves")
-            #sleep(0.2)
         elif lock == 10:
             bluePin1.off()
             redPin1.off()
             greenPin1.on()
-#             print("lock = 10 Deadzoning V")
             goodmove = pidevice.qVMO(('U',), (Umaxdist*(Uval-0.5),))
             if goodmove == False:
                 tar = pidevice.qTRA(('U',), (Umaxdist*(Uval-0.5),))
                 tar['U'] = truncate(tar['U'], 1)
-#                 print(tar)
-#                 print("U Distance: " + "{:1.2f}".format(Vval) + " m")
-#                 print("Moving U to: " + "{:1.2f}".format(tar['U']))
                 pidevice.MOV(('U',),(tar['U'],))
                 WaitForMotionDone(pidevice, 'U')
             else:
-#                 print("U Distance: " + "{:1.2f}".format(Uval) + " m")
-#                 print("Moving U to: " + "{:1.2f}".format(tar['U']))
                 pidevice.MOV(('U',),(Umaxdist*(Uval-0.5),))
                 WaitForMotionDone(pidevice, 'U')
-#             print("Finished Deadzone V Moves")
-            #sleep(0.2)
         else:
             pass
-             #print("Deadzone")
-#             print("Billys Great")
-#             print("Finished Deadzone")
-            #sleep(0.2)
         if lastUVal == Uval and lastVVal == Vval:
             counter += 1
         elif retrigger == 1: 
             counter = 200
         else: counter = 0
-#         print(counter)
         lastUVal, lastVVal = Uval, Vval
-#         print(lastUVal, lastVVal )
         if counter >= 200:
             redOff()
             greenOff()
@@ -216,17 +153,13 @@ def read_distance(pidevice, Umaxdist, Vmaxdist):
             isRunning = True
             while isRunning:
                 isRunning = pidevice.IsRunningMacro()
-#                 print(isRunning)
                 if isRunning == False: 
-                    #isRunning = True
                     retrigger = 1
                 Uval = Usensor.value
                 Vval = Vsensor.value
-#                 print(Uval,Vval)
                 if Uval > 0.075 and Vval > 0.075:
                     pass
                 else:
-#                     print("debugging")
                     pidevice.STP(noraise=True)
                     blueOff()
                     redOn()
@@ -259,7 +192,6 @@ def WaitForMotionDone(device, axis):
 
 def ConnectController():
     gateway = PISerial("/dev/ttyUSB0", 115200)
-    #gateway = PISocket('172.31.0.147', 50000)
     messages = GCSMessages(gateway)
     gcs = GCSCommands(gcsmessage = messages)
     gcs.SVO(AXES[0],1)
@@ -279,18 +211,7 @@ signal(SIGHUP, safe_exit)
 
 def main():
     yellowOn()
-    sleep(1)
-    yellowOff()
-    greenOn()
-    sleep(1)
-    greenOff()
-    redOn()
-    sleep(1)
-    redOff()
-    blueOn()
-    sleep(1)
     gateway, messages, pidevice = ConnectController()
-    print(pidevice.qIDN())
     #Transport Position Facilitation
     pidevice.MOV('X', 0)
     pidevice.MOV('Y', 0)
