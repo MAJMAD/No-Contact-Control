7e16c616aa4df73a61a65495ecf91d150808bcc3
Jacob Mader <91611605+MAJMAD@users.noreply.github.com>
2022-03-04 14:32:14 +0000

    Add files via upload

--- Sensor_Reading_rounded_averaged.ino
+++ Sensor_Reading_rounded_averaged.ino
@@ -1,4 +1,5 @@
-/*Jacob Mader*/
+/*Jacob Mader 3/4/2022*/
+/*This code facilitates use of the distance sensors to allow for accurate and real-time positioning of the hexapod by the rasp-pi
 /*define the sensor's pins*/
 uint8_t trigPin1 = 1;
 uint8_t echoPin1 = 0;
@@ -8,8 +9,9 @@ uint8_t echoPin2 = 12;
 /* define constants */
 const int TIMER_TRIGGER_HIGH = 1;
 const int TIMER_LOW_HIGH = 10;
-const int ARRAY_SIZE = 100;
-const int ARRAY_INIT_VAL = 0;
+const int ARRAY_SIZE = 50;
+const float ARRAY_INIT_VAL = 0;
+const int SENSOR_RANGE = 70;
 
 /*The states of an ultrasonic sensor*/
 enum SensorStates {
@@ -23,11 +25,10 @@ enum SensorStates {
 
 /* define globals */
 float timeDuration1, timeDuration2;
-int Sensor1Array[ARRAY_SIZE];
-int Sensor2Array[ARRAY_SIZE];
-int Sensor1FinalArray[400];
-int Sensor2FinalArray[400];
-int readcount = 0;
+float Sensor1Array[ARRAY_SIZE];
+float Sensor2Array[ARRAY_SIZE];
+float LastSensor1Value = 0.09;
+float LastSensor2Value = 0.09;
 unsigned long timerStart = 0;
 
 SensorStates _sensorState = TRIG_LOW1;
@@ -42,20 +43,20 @@ bool isTimerReady(int mSec) {
   return (millis() - timerStart) < mSec;
 }
 
-void initArray(int SensorArray[]) {
+void initArray(float SensorArray[]) {
   for (int i = 0; i < ARRAY_SIZE; i++) {
     SensorArray[i] = ARRAY_INIT_VAL;
   }
 }
 
-void updateArray(int SensorArray[], int updateValue) {
+void updateArray(float SensorArray[], float updateValue) {
   for (int i = 0; i < ARRAY_SIZE - 1; i++) {
     SensorArray[i] = SensorArray[i+1];
   }
   SensorArray[ARRAY_SIZE - 1] = updateValue;
 }
 
-void printArray(int SensorArray[]) {
+void printArray(float SensorArray[]) {
   for (int i = 0; i < ARRAY_SIZE; i++) {
     Serial.print(SensorArray[i]);
     Serial.print(" ");
@@ -63,7 +64,7 @@ void printArray(int SensorArray[]) {
   Serial.println("");
 }
 
-void printFinalArray(int SensorArray[]) {
+void printFinalArray(float SensorArray[]) {
   for (int i = 100; i < 400; i++) {
     Serial.print(SensorArray[i]);
     Serial.print(" ");
@@ -71,14 +72,34 @@ void printFinalArray(int SensorArray[]) {
   Serial.println("");
 }
 
-int avrArray(int SensorArray[]) {
-  long AvrValue = 0;
+float avrArray(float SensorArray[]) {
+  float AvrValue = 0;
   for (int i = 0; i < ARRAY_SIZE; i++) {
     AvrValue += SensorArray[i];
   }
-  return (int) (AvrValue / ARRAY_SIZE) ;
+  return (AvrValue / ARRAY_SIZE) ;
 }
 
+float checkSensor1Value(float Sensor1Value) {
+  if (Sensor1Value > SENSOR_RANGE*2/0.034) {
+    Sensor1Value = LastSensor1Value;
+    return Sensor1Value;
+  }
+  else {
+    LastSensor1Value = Sensor1Value/(SENSOR_RANGE*2/0.034);
+    return Sensor1Value/(SENSOR_RANGE*2/0.034);
+  }
+}
+float checkSensor2Value(float Sensor2Value) {
+  if (Sensor2Value > SENSOR_RANGE*2/0.034) {
+    Sensor2Value = LastSensor2Value;
+    return Sensor2Value;
+  }
+  else {
+    LastSensor2Value = Sensor2Value/(SENSOR_RANGE*2/0.034);
+    return Sensor2Value/(SENSOR_RANGE*2/0.034);
+   }
+}
 /*Sets the data rate in bits per second and configures the pins */
 void setup() {
   Serial.begin(9600);
@@ -115,16 +136,21 @@ void loop() {
     case ECHO_HIGH1: {
         digitalWrite(trigPin1, LOW);
         timeDuration1 = pulseIn(echoPin1, HIGH);
+        Serial.println(timeDuration1);
         timeDuration1 = (int) timeDuration1 / 10;
         timeDuration1 = timeDuration1 * 10;
+        Serial.println(timeDuration1);
+        timeDuration1 = checkSensor1Value(timeDuration1);
+        Serial.println(timeDuration1);
         updateArray(Sensor1Array, timeDuration1);
         timeDuration1 = avrArray(Sensor1Array);
+        Serial.print(timeDuration1);
         /*
            distance = time * speed of sound
            speed of sound is 340 m/s => 0.034 cm/us
         */
         Serial.print("stefan 1: ");
-        Serial.println(timeDuration1 * 0.034 / 2);
+        Serial.println(timeDuration1);
         //Serial.println(" cm");
         _sensorState = TRIG_LOW2;
       } break;
@@ -149,16 +175,21 @@ void loop() {
     case ECHO_HIGH2: {
         digitalWrite(trigPin2, LOW);
         timeDuration2 = pulseIn(echoPin2, HIGH);
+        Serial.println(timeDuration2);
         timeDuration2 = (int) timeDuration2 / 10;
         timeDuration2 = timeDuration2 * 10;
+        Serial.println(timeDuration2);
+        timeDuration2 = checkSensor2Value(timeDuration2);
+        Serial.println(timeDuration2);
         updateArray(Sensor2Array, timeDuration2);
         timeDuration2 = avrArray(Sensor2Array);
+        Serial.println(timeDuration2);
         /*
            distance = time * speed of sound
            speed of sound is 340 m/s => 0.034 cm/us
         */
         Serial.print("marcus 2: ");
-        Serial.println(timeDuration2 * 0.034 / 2);
+        Serial.println(timeDuration2);
         //Serial.println(" cm");
         _sensorState = TRIG_LOW1;
       } break;
